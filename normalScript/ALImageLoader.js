56093c4efff16ead43527d7a6378d119596ad782
Allen <wei125423412@163.com>
2019-07-18 15:40:02 +0800

    ALImageLoader Change

--- normalScript/ALImageLoader.js
+++ normalScript/ALImageLoader.js
@@ -1,8 +1,18 @@
 var ALImageLoader = (function(){
+    var ERROR = {
+        DOWNLOAD_ERROR: {code:-101,msg:"download file error"},     //下载错误
+        DOWNLOAD_FAILED : {code:-102,msg:"download file failed"},  //下载失败
+        REMOTE_FILE_ERROR: {code:-103,msg:"remote file error"},    //远端文件错误 
+        WRITE_FAILED : {code:-201,msg:"write file failed"},        //远端文件写入本地失败
+        READ_FAILED : {code:-202,msg:"read file failed"},          //加载本地文件失败
+    };
+
+
     // 数据配置
     var _config = {
         MAX_TASKS:5,      // 最大同步任务数量
     };
+
     var self = {
         // 默认配置
         _options:{
@@ -30,10 +40,7 @@ var ALImageLoader = (function(){
          * @param {*} url 
          */
         releaseImage:function(url){
-            let sf = self._query(url);
-            if(sf){
-                
-            }
+            self._remove(url);
         },
 
         
@@ -122,7 +129,7 @@ var ALImageLoader = (function(){
                 function loadEnd(){
                     cc.loader.load(filepath,function(err,texture){
                         if(err){
-                            self._doTaskFinish(err,url,null);
+                            self._doTaskFinish(ERROR.READ_FAILED,url,null);
                         }else{
                             let sf = new cc.SpriteFrame(texture,cc.Rect(0,0,texture.width,texture.height));
                             self._doTaskFinish(null,url,sf);
@@ -147,14 +154,12 @@ var ALImageLoader = (function(){
                             loadEnd();
                         }else{
                             cc.log("Remote write file failed");
-                            let error = {status:-100,errorMessage:"Remote write file failed"};
-                            self._doTaskFinish(error,url,null);
+                            self._doTaskFinish(ERROR.WRITE_FAILED,url,null);
                         }
                         
                     }else{
                         cc.log("Download file failed");
-                        let error = {status:-1,errorMessage:"Download file failed"}
-                        self._doTaskFinish(error,url,null);
+                        self._doTaskFinish(ERROR.REMOTE_FILE_ERROR,url,null);
                     }
                 }
 
@@ -167,13 +172,13 @@ var ALImageLoader = (function(){
                             var response = xhr.response;
                             saveFile(response);
                         }else{
-                            saveFile(null);
+                            self._doTaskFinish(ERROR.DOWNLOAD_ERROR,url,null);
                         }
                     }
                 }.bind(this);
                 let errCallback = function(event){
                     cc.log("image 请求出错 或者 超时");
-                    saveFile(null);
+                    self._doTaskFinish(ERROR.DOWNLOAD_FAILED,url,null);
                 }
                 xhr.responseType = 'arraybuffer';
                 xhr.ontimeout = errCallback;
@@ -184,7 +189,7 @@ var ALImageLoader = (function(){
             }else{
                 cc.loader.load(url,function(err,texture){
                     if(err){
-                        self._doTaskFinish(err,url,null);
+                        self._doTaskFinish(ERROR.READ_FAILED,url,null);
                     }else{
                         var sf = new cc.SpriteFrame(texture,cc.Rect(0,0,texture.width,texture.height));
                         self._doTaskFinish(null,url,sf);
@@ -206,43 +211,12 @@ var ALImageLoader = (function(){
             }
             // 移除任务列表中的任务
             try {
-                // let index = self._tasks.indexOf(url);
-                // if(index !== -1){
-                //     // 移除任务
-                //     self._tasks.splice(index,1);
-                //     // 执行回调函数
-                //     let callbacks = self._taskCallbacks[url];
-                //     if(callbacks && Array.isArray(callbacks)){
-                //         for(let i = 0; i < callbacks.length; ++i){
-                //             let cb = callbacks[i];
-                //             if(cb) cb(err,url,sf);
-                //         }
-                //     }
-                //     // 移除回调
-                //     delete self._taskCallbacks[url]
-
-                //     // 删除执行任务中的任务
-                //     let tIndex = self._run_tasks.indexOf(url);
-                //     if(tIndex !== -1){
-                //         self._run_tasks.splice(tIndex,1);
-                //     }else{
-                //         cc.error("当前任务的url 找不到");
-                //     }
-                // }else{
-                //     cc.error("找不到url");
-                // }
 
                 // 执行回调函数
                 let callbacks = self._taskCallbacks[url];
-                if(callbacks && Array.isArray(callbacks)){
-                    for(let i = 0; i < callbacks.length; ++i){
-                        let cb = callbacks[i];
-                        if(cb) cb(err,url,sf);
-                    }
-                }
+
                 // 移除回调
                 delete self._taskCallbacks[url]
-
                 // 删除执行任务中的任务
                 let tIndex = self._run_tasks.indexOf(url);
                 if(tIndex !== -1){
@@ -250,7 +224,14 @@ var ALImageLoader = (function(){
                 }else{
                     cc.error("当前任务的url 找不到");
                 }
-                
+
+                // 执行回调函数
+                if(callbacks && Array.isArray(callbacks)){
+                    for(let i = 0; i < callbacks.length; ++i){
+                        let cb = callbacks[i];
+                        if(cb) cb(err,url,sf);
+                    }
+                }
             } catch (error) {
                 cc.error(error);
             }
